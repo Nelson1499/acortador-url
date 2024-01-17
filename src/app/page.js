a1dfbebce9365f1400d1a7ba822432617454df95
Nelson1499 <avalosnelson9919@gmail.com>
2024-01-17 02:32:35 -0300

    front-end de la web acortadora de links

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,41 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import wave from "@/ilustraciones/wave.svg";
+import link1 from "@/ilustraciones/link-1.svg";
+import link2 from "@/ilustraciones/link-2.svg";
 
+export const metadata = {
+  icons: {
+    icon: "/icon.png",
+  },
+};
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className="contenedor">
+      <header>
+        <h1>Acortador de urls</h1>
+      </header>
+      <Image
+        className="image-link1"
+        src={link1}
+        alt="vector"
+        style={{ objectFit: "cover" }}
+      />
+      <form className="form-acortador">
+        <input type="text" placeholder="Acortar Url" />
+        <button>Acortar</button>
+      </form>
+      <Image
+        className="image-wave"
+        src={wave}
+        alt="vector"
+        style={{ objectFit: "cover" }}
+      />
+      <Image
+        className="image-link2"
+        src={link2}
+        alt="vector"
+        style={{ objectFit: "cover" }}
+      />
     </main>
-  )
+  );
 }
