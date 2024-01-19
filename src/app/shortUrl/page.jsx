909dc2a62e937a34c7af603560637bc811de1b0c
Nelson1499 <avalosnelson9919@gmail.com>
2024-01-19 01:37:59 -0300

    agregando toast, tambien algunas reglas para evitar errores

--- src/app/shortUrl/page.jsx
+++ src/app/shortUrl/page.jsx
@@ -1,4 +1,4 @@
-"use server"
+"use server";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
@@ -7,10 +7,19 @@ export const ShortUrl = async (formData) => {
   const url = formData.get("url");
   const shortUrl = Math.random().toString(36).substring(2, 5);
   try {
-    const data = await prisma.link.create({
-      data: { url, shortUrl },
-    })
-    return data
+    const result = await prisma.link.findUnique({
+      where: { url },
+    });
+    if (!result) {
+      const data = await prisma.link.create({
+        data: { url, shortUrl },
+      });
+      console.log(data)
+      return data;
+    }else{
+      console.log(result)
+      return result;
+    }
   } catch (error) {
     console.error(error);
   }
--- src/form/form-urls.jsx
+++ src/form/form-urls.jsx
@@ -1,27 +1,60 @@
 "use client";
+import CopyAllIcon from "@mui/icons-material/CopyAll";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import { ShortUrl } from "@/app/shortUrl/page";
 import Link from "next/link";
 import { useRef, useState } from "react";
-import { usePathname } from "next/navigation";
+import toast, { Toaster } from "react-hot-toast";
 
 export const FormUrl = () => {
   const [Urlshort, setUrlshort] = useState("");
-  const pathname = usePathname();
   const formRef = useRef();
   const shortuUrlNew = async (formData) => {
     const data = await ShortUrl(formData);
-    setUrlshort(data.shortUrl);
-    console.log(data);
+    console.log(data)
+    if (data) setUrlshort(data.shortUrl);
     formRef.current?.reset();
+  };
+  const clipboard = (copy) => {
+    navigator.clipboard
+      .writeText(copy)
+      .then(() => {
+        toast(`Copy url ${copy}`, {
+          duration: 4000,
+          position: 'top-center',
+        
+          icon: <CopyAllIcon />,
+        
+          iconTheme: {
+            primary: '#000',
+            secondary: '#fff',
+          },
+        
+          ariaProps: {
+            role: 'status',
+            'aria-live': 'polite',
+          },
+        });
+      })
+      .catch(function () {
+        console.error("Error al copiar texto al portapapeles");
+      });
   };
   return (
     <form ref={formRef} action={shortuUrlNew} className="form-acortador">
+      <Toaster position="top-center" reverseOrder={false} />
       <input type="text" name="url" id="url" placeholder="Acortar Url" />
       <button>Acortar</button>
-      <div>
-        Url:
-        <Link href={Urlshort}> {`${window.location.origin}/${Urlshort}`} </Link>
-      </div>
+      {Urlshort !== "" && (
+        <div className="container-shortlink">
+          Url:
+          <Link target="_blank" rel="noreferrer" href={Urlshort}>{`${window.location.origin}/${Urlshort}`}</Link>
+          <ContentCopyIcon
+            onClick={() => clipboard(`${window.location.origin}/${Urlshort}`)}
+            fontSize="medium"
+          />
+        </div>
+      )}
     </form>
   );
 };
